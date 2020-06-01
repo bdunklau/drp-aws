d4a2e46981c462737621f1d3f0051739bfd9953a
EC2 Default User <ec2-user@ip-172-31-28-156.us-east-2.compute.internal>
2020-06-01 19:01:19 +0000

    working on getting another test to pass individually

--- drp-client/src/app/toll/toll.component.spec.ts
+++ drp-client/src/app/toll/toll.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HttpClient  } from '@angular/common/http';
 import { TollComponent } from './toll.component';
 import { TollService } from '../toll.service';
@@ -13,7 +13,7 @@ fdescribe('TollComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ TollComponent ],
-      imports: [ FormsModule, ReactiveFormsModule, HttpClientModule ],
+      imports: [ FormsModule, HttpClientModule ],
       schemas: [ NO_ERRORS_SCHEMA ],
       providers: [TollService, HttpClient]
     })
