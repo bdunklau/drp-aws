75bbe726529b5aab310129ef34c7f6456b9e16b9
EC2 Default User <ec2-user@ip-172-31-28-156.us-east-2.compute.internal>
2020-06-01 16:46:40 +0000

    still trying

--- drp-client/src/app/toll.service.spec.ts
+++ drp-client/src/app/toll.service.spec.ts
@@ -1,11 +1,11 @@
 import { TestBed } from '@angular/core/testing';
-import { TollService } from './toll.service';
 import { HttpClient  } from '@angular/common/http';
 import { MockBackend } from '@angular/http/testing';
 import {
   Http, HttpModule, XHRBackend, ResponseOptions,
   Response, BaseRequestOptions
 } from '@angular/http';
+import { TollService } from './toll.service';
 
 
 fdescribe('TollService', () => {
