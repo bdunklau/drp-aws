6e4948171deda20a900e0b76ea1c9a26a60d7e29
EC2 Default User <ec2-user@ip-172-31-28-156.us-east-2.compute.internal>
2020-06-01 04:56:45 +0000

    still trying

--- drp-client/src/app/toll.service.spec.ts
+++ drp-client/src/app/toll.service.spec.ts
@@ -1,9 +1,11 @@
 import { TestBed } from '@angular/core/testing';
-
 import { TollService } from './toll.service';
+import { HttpClient } from '@angular/common/http';
 
 describe('TollService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    providers: [{provide: HttpClient, useValue: {}}]
+  }));
 
   it('should be created', () => {
     const service: TollService = TestBed.get(TollService);
