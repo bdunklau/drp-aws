593f1588c8d9ccd57dabad12cea3aec45cc7d996
EC2 Default User <ec2-user@ip-172-31-28-156.us-east-2.compute.internal>
2020-06-08 17:57:33 +0000

    previous build failed

--- drp-client/src/app/charge.service.spec.ts
+++ drp-client/src/app/charge.service.spec.ts
@@ -1,3 +1,4 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { HttpClientModule, HttpClient  } from '@angular/common/http';
 import { ChargeService } from './charge.service';
