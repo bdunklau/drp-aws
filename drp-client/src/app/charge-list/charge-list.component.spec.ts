991181f3e7e7f2a7dde4e7168e59103e174b0549
EC2 Default User <ec2-user@ip-172-31-28-156.us-east-2.compute.internal>
2020-06-08 18:06:01 +0000

    previous build failed

--- drp-client/src/app/charge-list/charge-list.component.spec.ts
+++ drp-client/src/app/charge-list/charge-list.component.spec.ts
@@ -1,6 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { HttpClientModule, HttpClient  } from '@angular/common/http';
 import { ChargeListComponent } from './charge-list.component';
+import { ChargeService } from '../charge.service';
+
+
 
 describe('ChargeListComponent', () => {
   let component: ChargeListComponent;
@@ -8,7 +12,31 @@ describe('ChargeListComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ChargeListComponent ]
+      declarations: [ ChargeListComponent ],,
+      imports: [ HttpClientModule ],
+      schemas: [ NO_ERRORS_SCHEMA ],
+      providers: [ChargeService, HttpClient,
+                  {
+                    provide: Router,
+                    useClass: class { navigate = jasmine.createSpy("navigate"); }
+                  },
+
+            {
+              provide: ActivatedRoute,
+              useValue: {
+                snapshot: {
+                  paramMap:{get: function(str) {return '123'} }
+                },
+                paramMap: {
+                  subscribe: function(params:ParamMap) {
+                  }
+                }
+              }
+             }
+
+      ]
+
+
     })
     .compileComponents();
   }));
