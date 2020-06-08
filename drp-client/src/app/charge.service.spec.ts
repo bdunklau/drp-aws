ecddbc5d9eb6bae7b0c34589411f62d02fe084f1
EC2 Default User <ec2-user@ip-172-31-28-156.us-east-2.compute.internal>
2020-06-08 17:52:49 +0000

    previous build failed

--- drp-client/src/app/charge.service.spec.ts
+++ drp-client/src/app/charge.service.spec.ts
@@ -1,9 +1,25 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientModule, HttpClient  } from '@angular/common/http';
 import { ChargeService } from './charge.service';
 
 describe('ChargeService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+
+
+    beforeEach(async () => {
+       TestBed.configureTestingModule({
+         declarations: [ ],
+         imports: [
+           HttpClientModule
+         ],
+         schemas: [NO_ERRORS_SCHEMA],
+         providers: [
+           HttpClient,
+         ]
+       })
+         .compileComponents();
+    });
+
+
 
   it('should be created', () => {
     const service: ChargeService = TestBed.get(ChargeService);
