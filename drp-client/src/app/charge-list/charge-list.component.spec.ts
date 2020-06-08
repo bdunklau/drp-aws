4227926cc1fd23d779254100a88f0babf37d95fa
EC2 Default User <ec2-user@ip-172-31-28-156.us-east-2.compute.internal>
2020-06-08 18:16:35 +0000

    previous build failed

--- drp-client/src/app/charge-list/charge-list.component.spec.ts
+++ drp-client/src/app/charge-list/charge-list.component.spec.ts
@@ -16,24 +16,6 @@ describe('ChargeListComponent', () => {
       imports: [ HttpClientModule ],
       schemas: [ NO_ERRORS_SCHEMA ],
       providers: [ChargeService, HttpClient,
-                  {
-                    provide: Router,
-                    useClass: class { navigate = jasmine.createSpy("navigate"); }
-                  },
-
-            {
-              provide: ActivatedRoute,
-              useValue: {
-                snapshot: {
-                  paramMap:{get: function(str) {return '123'} }
-                },
-                paramMap: {
-                  subscribe: function(params:ParamMap) {
-                  }
-                }
-              }
-             }
-
       ]
 
 
